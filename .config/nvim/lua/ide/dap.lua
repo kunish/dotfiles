457841978dde812cd01c5b4f4154682f7ba8275c
kunish <kunish.butt@gmail.com>
2021-12-08 15:08:17 +0800

    feat: add .wgetrc

--- .config/nvim/lua/ide/dap.lua
+++ .config/nvim/lua/ide/dap.lua
@@ -1,11 +1,14 @@
 local M = {}
 
 local dap_install = require 'dap-install'
+local dbg_list = require('dap-install.api.debuggers').get_installed_debuggers()
 
 function M.setup()
-  dap_install.setup {
-    installation_path = vim.fn.stdpath 'data' .. '/dapinstall',
-  }
+  dap_install.setup()
+
+  for _, debugger in ipairs(dbg_list) do
+    dap_install.config(debugger)
+  end
 end
 
 return M
