300678aff9c5dfa956b42e4477ea8ee283c9a6ac
kunish <kunish.butt@gmail.com>
2021-12-14 16:34:28 +0800

    feat(nvim): recognize mdx file as markdown

--- .config/nvim/lua/core/ft.lua
+++ .config/nvim/lua/core/ft.lua
@@ -5,6 +5,9 @@ local filetype = require('filetype')
 function M.setup()
   filetype.setup({
     overrides = {
+      extensions = {
+        mdx = 'markdown',
+      },
       literal = {
         ['.eslintcache'] = 'json',
         ['.releaserc'] = 'json',
