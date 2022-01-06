e66f5290a184bae69706ad13c8785bf4056538df
kunish <kunish.butt@gmail.com>
2022-01-06 17:41:23 +0800

    feat(term): add wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+
+return {
+	font = wezterm.font("UbuntuMono Nerd Font", { bold = true }),
+	font_size = 14,
+	color_scheme = "Gruvbox Dark",
+	enable_scroll_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	window_decorations = "RESIZE",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+}
