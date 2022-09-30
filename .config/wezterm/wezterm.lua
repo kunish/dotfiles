cd3ba71dc510319b5079b73eb408a1c94592781a
shikun <kunish.butt@gmail.com>
2022-09-30 11:24:40 +0800

    feat: update resources

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,4 +7,11 @@ return {
 	color_scheme = "ayu",
 	enable_scroll_bar = false,
 	hide_tab_bar_if_only_one_tab = true,
+	keys = {
+		{
+			key = "w",
+			mods = "SUPER",
+			action = wezterm.action.CloseCurrentPane({ confirm = true }),
+		},
+	},
 }
