c16ab2ea9ba6b881b5ce8d1bab4224383a71655c
kunish <kunish.butt@gmail.com>
2023-02-04 21:40:50 +0800

    feat: update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,11 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font("UbuntuMono Nerd Font", { bold = true }),
+	font = wezterm.font("UbuntuMono Nerd Font", { bold = true, italic = true }),
 	audible_bell = "Disabled",
-	font_size = 14,
-	color_scheme = "OneDark (Gogh)",
+	font_size = 15,
+  line_height = 1.3,
+	color_scheme = "OneHalfBlack (Gogh)",
 	enable_scroll_bar = false,
 	hide_tab_bar_if_only_one_tab = true,
 	keys = {
