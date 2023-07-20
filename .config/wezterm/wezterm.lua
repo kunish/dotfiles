813675e5f068095158c0722e2555a1c74a61927e
kunish <kunish.butt@gmail.com>
2023-07-20 13:26:25 +0800

    feat: update config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,33 +1,33 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font_with_fallback({
-		{
-			family = "Comic Code Ligatures",
-			weight = "Bold",
-			harfbuzz_features = { "liga=0" },
-		},
-		{
-			family = "小賴字體 等寬 SC",
-			weight = "Bold",
-		},
-	}),
-	font_size = 12,
-	line_height = 1.4,
-	color_scheme = "OneHalfBlack (Gogh)",
-	audible_bell = "Disabled",
-	enable_scroll_bar = false,
-	hide_tab_bar_if_only_one_tab = true,
-	keys = {
-		{
-			key = "q",
-			mods = "CTRL|SHIFT",
-			action = wezterm.action.QuickSelect,
-		},
-		{
-			key = "w",
-			mods = "SUPER",
-			action = wezterm.action.CloseCurrentPane({ confirm = true }),
-		},
-	},
+  font = wezterm.font_with_fallback({
+    {
+      family = "Comic Code Ligatures",
+      weight = "Bold",
+      -- harfbuzz_features = { "liga=0" },
+    },
+    {
+      family = "小賴字體 等寬 SC",
+      weight = "Bold",
+    },
+  }),
+  font_size = 12,
+  line_height = 1.4,
+  color_scheme = "OneHalfBlack (Gogh)",
+  audible_bell = "Disabled",
+  enable_scroll_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+  keys = {
+    {
+      key = "q",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action.QuickSelect,
+    },
+    {
+      key = "w",
+      mods = "SUPER",
+      action = wezterm.action.CloseCurrentPane({ confirm = true }),
+    },
+  },
 }
